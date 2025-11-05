c89046be54545697b2814952a571c22873d81a86
skandinavia05 <skandinavia05@gmail.com>
2025-11-05 23:23:55 +0700

    tambah 01_length_size

---
+++ 01_length_size.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "air mineral";
+
+    cout << "Teks: " << teks << endl;
+    cout << "length(): " << teks.length() << endl;
+    cout << "size(): " << teks.size() << endl;
+
+    return 0;
+}
