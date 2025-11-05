ee0bef3667fb147c6913cf90d992ed2208f6b283
skandinavia05 <skandinavia05@gmail.com>
2025-11-05 23:55:43 +0700

    tambah 04_find

---
+++ 04_find.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string kalimat = "saya membeli nasi goreng";
+    int posisi = kalimat.find("nasi");
+
+    cout << posisi << endl;
+    return 0;
+}
